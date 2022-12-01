1945f62270f4273ccd3f9ff4f16bebafbe9af4fa
Viktor <kolo.eri@gmail.com>
2022-12-01 20:53:00 +0100

    Added BUtton logik

--- MTG-HT/Game1.cs
+++ MTG-HT/Game1.cs
@@ -9,11 +9,14 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     UI _ui; 
+    Button A;
 
     int _width = 0;
     int _height = 0;
+    string temp = "";
     Vector2 MousePos;
     SpriteFont font;
+    Texture2D Ryan; // A naughty Temp
 
     public Game1()
     {
@@ -37,6 +40,8 @@ public class Game1 : Game
         _graphics.IsFullScreen = true;
         _graphics.ApplyChanges();
 
+        A = new Button(new Vector2(500, 500), new Vector2(750, 750));
+
         base.Initialize();
     }
 
@@ -46,6 +51,7 @@ public class Game1 : Game
 
         // TODO: use this.Content to load your game content here
         font = this.Content.Load<SpriteFont>("Fonts/SourceCodePro");
+        Ryan = this.Content.Load<Texture2D>("Sprites/Button_Temp");
     }
 
     protected override void Update(GameTime gameTime)
@@ -59,6 +65,11 @@ public class Game1 : Game
         MouseState mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
         MousePos = new Vector2(mouseState.X, mouseState.Y);
 
+        if (_ui.LMH)
+            temp = A.clicked(MousePos).ToString();
+        else
+            temp = "False";
+
         base.Update(gameTime);
     }
 
@@ -71,6 +82,9 @@ public class Game1 : Game
 
         _spriteBatch.DrawString(font, "Mouse Position: " + MousePos.X + " " + MousePos.Y, new Vector2(50, 50), Color.Red);
         _spriteBatch.DrawString(font, "RM: " + _ui.RMC.ToString() + _ui.RMH.ToString() + " LM: " + _ui.LMC.ToString() + _ui.LMH.ToString(), new Vector2(50, 100), Color.Red);
+        _spriteBatch.DrawString(font, "Buuton is pressed?: " + temp, new Vector2(50, 150), Color.Red);
+
+        _spriteBatch.Draw(Ryan, new Rectangle((int)A.UL.X, (int)A.UL.Y, (int)(A.DR.X - A.UL.X), (int)(A.DR.Y - A.UL.Y)), new Rectangle(0, 0, Ryan.Width, Ryan.Height), Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- MTG-HT/UI.cs
+++ MTG-HT/UI.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace MTG_HT
@@ -18,6 +19,34 @@ namespace MTG_HT
             if (!LMH && Mouse.GetState().LeftButton == ButtonState.Pressed) {LMC = true; LMH = true;}
             else if (LMC && LMH && Mouse.GetState().LeftButton == ButtonState.Pressed) {LMC = false;}
             else if (Mouse.GetState().LeftButton == ButtonState.Released) {LMC =false; LMH = false;}
+        }
+    }
+
+    class Button
+    {
+        public Vector2 UL {get;}
+        public Vector2 DR {get;}
+
+        public Button(Vector2 UL, Vector2 DR)
+        {
+            this.UL = UL;
+            this.DR = DR;
+        }
+
+        public bool clicked (Vector2 mousePos)
+        {
+            bool colide = true;
+
+            if (UL.X > mousePos.X)
+                colide = false;
+            if (UL.Y > mousePos.Y)
+                colide = false;
+            if (DR.X < mousePos.X)
+                colide = false;
+            if (DR.Y < mousePos.Y)
+                colide = false;
+            
+            return colide;
         }
     }
 }
