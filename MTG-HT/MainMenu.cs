9c9352842a7fdf4f1239a234b9290a18a296cfb0
Viktor <kolo.eri@gmail.com>
2022-12-04 10:12:54 +0100

    starting adding MainMenu

---
+++ MTG-HT/MainMenu.cs
@@ -0,0 +1,13 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MTG_HT;
+
+class MainMenu
+{
+    MainMenu ()
+    {
+
+    }
+}
